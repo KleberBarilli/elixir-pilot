cd51019d7067d03991a3207c079e97be21b3115b
kleberbarilli <kleber.barilli73@gmail.com>
2022-08-24 22:30:00 -0300

    usando pattern match p/ retornos quando um numero é multiplo por 3,5 ou 3 e 5

--- lib/hello.ex
+++ lib/hello.ex
@@ -8,8 +8,18 @@ defmodule Hello do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
   def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate_numbers(elem) do
+    number = String.to_integer(elem)
+    evaluate_numbers(number)
+  end
+
+  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :kb
+  def evaluate_numbers(number) when rem(number, 3) == 0, do: :k
+  def evaluate_numbers(number) when rem(number, 5) == 0, do: :b
+  def evaluate_numbers(number), do: number
 end
