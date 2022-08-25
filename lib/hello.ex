786c3b684226eaceb29948d61400c41659509c3a
kleberbarilli <kleber.barilli73@gmail.com>
2022-08-24 22:33:03 -0300

    usando funções privadas

--- lib/hello.ex
+++ lib/hello.ex
@@ -5,22 +5,22 @@ defmodule Hello do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
+  defp handle_file_read({:ok, result}) do
     result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 
-  def convert_and_evaluate_numbers(elem) do
+  defp convert_and_evaluate_numbers(elem) do
     elem
     |> String.to_integer()
     |> evaluate_numbers()
   end
 
-  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :kb
-  def evaluate_numbers(number) when rem(number, 3) == 0, do: :k
-  def evaluate_numbers(number) when rem(number, 5) == 0, do: :b
-  def evaluate_numbers(number), do: number
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :kb
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :k
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :b
+  defp evaluate_numbers(number), do: number
 end
