caaff61b830df8913b8f43fe334eaa4aa324b8d4
kleberbarilli <kleber.barilli73@gmail.com>
2022-08-24 22:44:45 -0300

    caso de teste de sucesso

--- test/hello_test.exs
+++ test/hello_test.exs
@@ -1,8 +1,10 @@
 defmodule HelloTest do
   use ExUnit.Case
-  doctest Hello
 
-  test "greets the world" do
-    assert Hello.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :k, 4, :b, :k, 8, 11, :kb, :b, :kb, :kb, :kb]}
+      assert Hello.build("numbers.txt") == expected_response
+    end
   end
 end
