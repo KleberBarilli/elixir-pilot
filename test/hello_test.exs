f09d89ec2ef112d545c2a8b486a7fef31f1a0c3c
kleberbarilli <kleber.barilli73@gmail.com>
2022-08-24 22:46:44 -0300

    caso de teste de falha

--- test/hello_test.exs
+++ test/hello_test.exs
@@ -6,5 +6,10 @@ defmodule HelloTest do
       expected_response = {:ok, [1, 2, :k, 4, :b, :k, 8, 11, :kb, :b, :kb, :kb, :kb]}
       assert Hello.build("numbers.txt") == expected_response
     end
+
+    test "when a invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert Hello.build("notfound.txt") == expected_response
+    end
   end
 end
