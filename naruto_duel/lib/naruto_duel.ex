9b918586006f623bc25c51d03f33c34b340107ef
kleberbarilli <kleber.barilli73@gmail.com>
2022-08-28 19:40:28 -0300

    naruto duel module

---
+++ naruto_duel/lib/naruto_duel.ex
@@ -0,0 +1,18 @@
+defmodule NarutoDuel do
+  @moduledoc """
+  Documentation for `NarutoDuel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NarutoDuel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ naruto_duel/test/naruto_duel_test.exs
@@ -0,0 +1,8 @@
+defmodule NarutoDuelTest do
+  use ExUnit.Case
+  doctest NarutoDuel
+
+  test "greets the world" do
+    assert NarutoDuel.hello() == :world
+  end
+end
