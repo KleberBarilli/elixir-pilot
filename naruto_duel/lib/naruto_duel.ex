27ae467f624caff30d7ac8b501395ec94b83963b
kleberbarilli <kleber.barilli73@gmail.com>
2022-09-04 19:21:53 -0300

    start fight

--- naruto_duel/lib/naruto_duel.ex
+++ naruto_duel/lib/naruto_duel.ex
@@ -1,7 +1,13 @@
 defmodule NarutoDuel do
-  alias NarutoDuel.Ninja
+  alias NarutoDuel.{Ninja, Game}
 
   def create_ninja(name, atk, def, heal) do
     Ninja.build(name, atk, def, heal)
+  end
+
+  def start_game(player) do
+    "Madara"
+    |> create_ninja(:katon, :rinnegan, :heal)
+    |> Game.start(player)
   end
 end
